0011024ee883b28569c8d89ace0fe657706defdd
Elijah Mooring <ejsisgreat@gmail.com>
2022-05-04 13:03:55 -0500

    first commit

---
+++ src/routes/blog/[post].svelte
@@ -0,0 +1,15 @@
+<script lang="ts" context="module">
+	import type { Load } from '@sveltejs/kit';
+
+	const load: Load = ({ params }) => {
+		return { props: params };
+	};
+</script>
+
+<script lang="ts">
+	export let post: string;
+</script>
+
+Hello, this is post {post}
+
+<a href="/">Back</a>
---
+++ src/routes/index.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	let post = '';
+</script>
+
+<input bind:value={post} />
+
+<a href="/blog/{post}">Go to post</a>
